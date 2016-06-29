24f71c59bd1f0a1264e9f505522c5cc59c9164c4
mtrefzer <mt@trefzer.it>
2016-06-29 15:26:46 +0200

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+## **What is phpMussel?**
+
+An ideal solution for shared hosting environments, where it's often not possible to utilise or install conventional anti-virus protection solutions, phpMussel is a PHP script designed to **detect trojans, viruses, malware and other threats** within files uploaded to your system wherever the script is hooked, based on the signatures of [ClamAV](http://www.clamav.net/) and others.
+
+---
+
+## **What's this repository for?**
+
+This repository, "plugin-resultAsJson", is the repository for a phpMussel plugin that allows you receive the detection output in JSON format.
+
+The core phpMussel repository: [phpMussel](https://github.com/Maikuolan/phpMussel).
+
+---
+
+## **How to install?**
+
+Add the following section to your `phpmussel.ini` file and edit accordingly:
+
+```ini
+[resultAsJson]
+; Output the result as JSON, default = false
+use_json=true
+```
+
+Upload the "resultAsJson" directory of this repository and all its contents to the "plugins" directory of your phpMussel installation (the "plugins" directory is a sub-directory of the "vault" directory).
+
+Instead of the html result you receive JSON:
+
+```json
+{ 
+	"origin" : "IP address",
+	"objects_scanned" : 1,
+	"detections_count" : 1,
+	"scan_errors" : 0,
+	"killdata" : "MD5 signature reconstruction (File-HASH:File-Size:File-Name)",
+	"detections" : "text whyflagged" 
+}
+```
+
+That's everything! :-)
+
+---
+
+*This file, "README.md", last edited: 29th June 2016 (2016.06.29).*
---
+++ resultAsJson/plugin.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Plugin resultAsJson for phpMussel.
+ *
+ * PLUGIN INFORMATION BEGIN
+ *         Plugin Name: resultAsJson.
+ *       Plugin Author: Michael Trefzer.
+ *      Plugin Version: 1.0.0
+ *    Download Address: https://github.com/mtrefzer/plugin-resultAsJson
+ *     Min. Compatible: 1.0.0-DEV
+ *     Max. Compatible: -
+ *        Tested up to: 1.0.0-DEV
+ *       Last Modified: 2016.06.29
+ * PLUGIN INFORMATION END
+ *
+ * This plugin sends the detection data as JSON response.
+ */
+/**
+ * Prevents direct access (the plugin should only be called from the phpMussel
+ * plugin system).
+ */
+if (!defined('phpMussel')) {
+    die('[phpMussel] This should not be accessed directly.');
+}
+/** Fallback for missing "resultAsJson" configuration category. */
+if (!isset($phpMussel['Config']['resultAsJson'])) {
+    $phpMussel['Config']['resultAsJson'] = array();
+}
+/** Fallback for missing "use_json" configuration directive. */
+if (!isset($phpMussel['Config']['resultAsJson']['use_json'])) {
+    $phpMussel['Config']['resultAsJson']['use_json'] = false;
+}
+/** Fallback for missing "use_json_flag" configuration directive. */
+if (!isset($phpMussel['Config']['resultAsJson']['use_json_flag'])) {
+    $phpMussel['Config']['resultAsJson']['use_json_flag'] = '';
+}
+/**
+ * Registers the `phpMussel_resultAsJson` closure to the `before_html_out`
+ * hook.
+ */
+$phpMussel['Register_Hook']('phpMussel_resultAsJson', 'before_html_out');
+
+/**
+ * @return bool Returns always true, because only Headers are set and the html output is changed
+ */
+$phpMussel_resultAsJson = function () use (&$phpMussel, &$html) {
+    if ($phpMussel['Config']['resultAsJson']['use_json'] &&
+        (strlen($phpMussel['Config']['resultAsJson']['use_json_flag']) == 0
+            || isset($_REQUEST[$phpMussel['Config']['resultAsJson']['use_json_flag']]))
+    ) {
+        $content = array('origin'           => $_SERVER[$phpMussel['Config']['general']['ipaddr']],
+                         'objects_scanned'  => $phpMussel['memCache']['objects_scanned'],
+                         'detections_count' => $phpMussel['memCache']['detections_count'],
+                         'scan_errors'      => $phpMussel['memCache']['scan_errors'],
+                         'killdata'         => $phpMussel['killdata'],
+                         'detections'       => trim($phpMussel['whyflagged']));
+
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+        header('Content-type: application/json');
+        $html = json_encode($content);
+    }
+    return true;
+};
